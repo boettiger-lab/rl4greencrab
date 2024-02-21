9100c3cc792d45a3945cb788caed50f020562fb7
Felipe Montealegre-Mora <felimomouni@gmail.com>
2024-02-21 01:15:41 +0000

    training metaenvs (such as time-series envs) from yaml files

--- src/rl4greencrab/util.py
+++ src/rl4greencrab/util.py
@@ -1,5 +1,7 @@
 import yaml
 import os
+
+import gymnasium as gym
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3 import PPO, A2C, DQN, SAC, TD3
 from sb3_contrib import TQC, ARS
@@ -42,9 +44,9 @@ def sb3_train(config_file):
         use_sde=options["use_sde"],
     )
 
-    model.learn(total_timesteps=options["total_timesteps"], tb_log_name=model_id)
+    model.learn(total_timesteps=options["total_timesteps"], tb_log_name=model_id, progress_bar=True)
 
-    os.mkdirs(options["save_path"], exist_ok=True)
+    os.makedirs(options["save_path"], exist_ok=True)
     model.save(save_id)
     try:
         full_path = save_id + ".zip"
@@ -71,5 +73,43 @@ def sb3_train_v2(options = dict):
 
     model.save(model_id)
     path = model_id + ".zip"
+    # deploy_model(path, "sb3/"+path, repo=options["repo"])
+    # deploy_model(config_file, "sb3/"+config_file, repo=options["repo"])
+
+def sb3_train_metaenv(config_file):
+    with open(config_file, "r") as stream:
+        options = yaml.safe_load(stream)
+    
+    vec_env = make_vec_env(
+        options["meta_env_id"], 
+        options["n_envs"], 
+        env_kwargs={
+            "config": 
+            {
+                "base_env": gym.make(options["base_env_id"], config=options["base_env_cfg"]), 
+                **options["meta_env_kwargs"],
+            }
+        }
+    )
+    ALGO = algorithm(options["algo"])
+    model_id = "{}-Meta_{}-Base_{}-{}".format(
+        options["algo"], 
+        options["meta_env_id"], 
+        options["base_env_id"], 
+        options["id"],
+    )
+    save_id = os.path.join(options["save_path"], model_id)
+
+    model = ALGO(
+        "MlpPolicy",
+        vec_env,
+        verbose=0,
+        tensorboard_log=options["tensorboard"],
+        use_sde=options["use_sde"],
+    )
+    model.learn(total_timesteps=options["total_timesteps"], tb_log_name=model_id, progress_bar=True)
+
+    model.save(save_id)
+    # path = model_id + ".zip"
     # deploy_model(path, "sb3/"+path, repo=options["repo"])
     # deploy_model(config_file, "sb3/"+config_file, repo=options["repo"])
