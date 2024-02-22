7b21c8ff78ef1b5bb5f43ca6fc69e5f9504e06d9
Felipe Montealegre-Mora <felimomouni@gmail.com>
2024-02-22 23:33:26 +0000

    benchmarks script

--- scripts/train_benchmarks.sh
+++ scripts/train_benchmarks.sh
@@ -4,7 +4,8 @@
 scriptdir="$(dirname "$0")"
 cd "$scriptdir"
 
-python train.py --file ../hyperpars/systematic-benchmarks/TQC-Nmem_1/6-tqc_nmem-1_bmk.yml &
-python train.py --file ../hyperpars/systematic-benchmarks/TQC-Nmem_1/7-tqc_nmem-1_bmk.yml &
-python train.py --file ../hyperpars/systematic-benchmarks/TQC-Nmem_1/8-tqc_nmem-1_bmk.yml &
-python train.py --file ../hyperpars/systematic-benchmarks/TQC-Nmem_1/9-tqc_nmem-1_bmk.yml &
+python train.py --file ../hyperpars/systematic-benchmarks/TQC-Nmem_1/10-tqc_nmem-1_bmk.yml &
+python train.py --file ../hyperpars/systematic-benchmarks/TQC-Nmem_1/11-tqc_nmem-1_bmk.yml &
+python train.py --file ../hyperpars/systematic-benchmarks/TQC-Nmem_1/12-tqc_nmem-1_bmk.yml &
+python train.py --file ../hyperpars/systematic-benchmarks/TQC-Nmem_1/13-tqc_nmem-1_bmk.yml &
+python train.py --file ../hyperpars/systematic-benchmarks/TQC-Nmem_1/14-tqc_nmem-1_bmk.yml &
