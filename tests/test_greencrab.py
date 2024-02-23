e234a0c8ead7245e98556b82de160c0b994faa42
Felipe Montealegre-Mora <felimomouni@gmail.com>
2024-02-23 00:50:20 +0000

    typo

--- tests/test_greencrab.py
+++ tests/test_greencrab.py
@@ -1,8 +1,8 @@
 # Confirm environment is correctly defined:
 from stable_baselines3.common.env_checker import check_env
-from rl4greencrab import greenCrabEnv, greenCrabSimplifiedEnv, TimeSeriesEnv
+from rl4greencrab import greenCrabEnv, greenCrabSimplifiedEnv, timeSeriesEnv
 
 def test_GC():
     check_env(greenCrabEnv(), warn=True)
     check_env(greenCrabSimplifiedEnv(), warn=True)
-    check_env(TimeSeriesEnv(), warn=True)
+    check_env(timeSeriesEnv(), warn=True)
