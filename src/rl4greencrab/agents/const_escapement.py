5c545af1179abeaab4785584cecd4e5295405560
Felipe Montealegre-Mora <felimomouni@gmail.com>
2024-02-25 01:39:36 +0000

    typo

--- src/rl4greencrab/agents/const_escapement.py
+++ src/rl4greencrab/agents/const_escapement.py
@@ -8,7 +8,7 @@ class constEsc:
 
     def predict(self, observation):
         obs_nat_units = self.bound * self.to_01(observation)
-        if obs_nat_units <= self.escapement or obs_nat_units =< 0:
+        if obs_nat_units <= self.escapement or obs_nat_units <= 0:
             return -1
         mortality = (obs_nat_units - self.escapement) / self.escapement
         return self.to_pm1(mortality)   
