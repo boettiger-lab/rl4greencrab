ec376ad1e9439d35931a2d8778c729038600f9aa
Felipe Montealegre-Mora <felimomouni@gmail.com>
2024-02-25 08:04:03 +0000

    more intro

--- src/rl4greencrab/__init__.py
+++ src/rl4greencrab/__init__.py
@@ -2,6 +2,7 @@ from rl4greencrab.envs.green_crab_ipm import greenCrabEnv, greenCrabSimplifiedEn
 from rl4greencrab.envs.time_series import timeSeriesEnv
 from rl4greencrab.agents.const_action import constAction
 from rl4greencrab.agents.const_escapement import constEsc
+from rl4greencrab.util.simulate import simulator
 # from envs.util import sb3_train, sb3_train_v2, sb3_train_metaenv
 
 from gymnasium.envs.registration import register
