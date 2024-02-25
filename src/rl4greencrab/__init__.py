5a5a7af81b53988258a465d74a1f0deb7375f4f0
Felipe Montealegre-Mora <felimomouni@gmail.com>
2024-02-25 01:36:45 +0000

    typo

--- src/rl4greencrab/__init__.py
+++ src/rl4greencrab/__init__.py
@@ -1,7 +1,7 @@
 from rl4greencrab.envs.green_crab_ipm import greenCrabEnv, greenCrabSimplifiedEnv
 from rl4greencrab.envs.time_series import timeSeriesEnv
-from rl4greengrab.agents.const_action import constAction
-from rl4greengrab.agents.const_escapement import constEsc
+from rl4greencrab.agents.const_action import constAction
+from rl4greencrab.agents.const_escapement import constEsc
 # from envs.util import sb3_train, sb3_train_v2, sb3_train_metaenv
 
 from gymnasium.envs.registration import register
