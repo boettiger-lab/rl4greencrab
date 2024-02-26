72e2cf805f13306f68f40d9fc96694866d636ac8
Felipe Montealegre-Mora <felimomouni@gmail.com>
2024-02-26 19:50:21 +0000

    train_algos bash script, train_gcse_manual python script, yaml configs

---
+++ scripts/train_algos.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# move to script directory for normalized relative paths.
+scriptdir="$(dirname "$0")"
+cd "$scriptdir"
+
+python train_gcse_manual.py -t 1000000 -a ppo -ne 10 &
+python train_gcse_manual.py -t 1000000 -a rppo -ne 10 &
+python train_gcse_manual.py -t 1000000 -a her -ne 10 &
+python train_gcse_manual.py -t 1000000 -a tqc -ne 10 &
+python train_gcse_manual.py -t 1000000 -a td3 &
