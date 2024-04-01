db5187f9389f60fe7c355e51fca832e983246408
Felipe Montealegre-Mora <felimomo@berkeley.edu>
2024-04-01 17:35:43 +0000

    const_action has a natural units agent now

--- src/rl4greencrab/agents/const_action.py
+++ src/rl4greencrab/agents/const_action.py
@@ -1,7 +1,15 @@
 class constAction:
-    def __init__(self, mortality=0, env = None, **kwargs):
-        self.mortality = mortality
-        self.action = 2 * self.mortality - 1
+    def __init__(self, action=-1, env = None, **kwargs):
+        self.action = action
+        self.env = env
+
+    def predict(self, observation, **kwargs):
+        return self.action, {}
+
+class constActionNatUnits:
+    def __init__(self, ntraps=0, env = None, **kwargs):
+        self.ntraps = ntraps
+        self.action = ntraps
         self.env = env
 
     def predict(self, observation, **kwargs):
