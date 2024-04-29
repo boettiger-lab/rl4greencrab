b6c471b50ec79adaeed2c1221df9c3b43bcc9b68
Felipe Montealegre-Mora <felimomo@berkeley.edu>
2024-04-29 19:57:42 +0000

    reward balancing

--- src/rl4greencrab/envs/green_crab_nine_month.py
+++ src/rl4greencrab/envs/green_crab_nine_month.py
@@ -1,3 +1,9 @@
+from gymnasium import spaces
+import numpy as np
+from scipy.stats import norm
+
+from rl4greencrab import greenCrabEnv
+
 class greenCrabNineMonth(greenCrabEnv):
     def __init__(self, config={}):
         super().__init__(config=config)
@@ -9,10 +15,12 @@ class greenCrabNineMonth(greenCrabEnv):
             np.float32(9 * [-1]),
             np.float32(9 * [1]),
         )
+        self.action_reward_scale = config.get("action_reward_scale", 0.05)
+        self.max_action = 10_000
 
     def reset(self):
-        unnorm_obs, info = super().reset(self)
-        norm_obs = 2 * unnorm_obs / self.max_observation - 1
+        unnorm_obs, info = super().reset()
+        norm_obs = 2 * unnorm_obs / self.max_obs - 1
         return norm_obs, info
 
     def step(self,action):
@@ -39,7 +47,7 @@ class greenCrabNineMonth(greenCrabEnv):
         
         #calculate removed and record observation at t=0
         #apply monthly harvest rate
-        removed[:,0] = [np.random.binomial(size_freq[k,0], harvest_rate[k, 0]) for k in range(self.nsize)]
+        removed[:,0] = [np.random.binomial(size_freq[k,0], harvest_rate[0, k]) for k in range(self.nsize)]
             
         #record the catch in the observation space
         self.observations[0] = np.sum(removed[:,0])
@@ -51,9 +59,11 @@ class greenCrabNineMonth(greenCrabEnv):
             n_j = self.gm_ker@(size_freq[:,j] - removed[:,j])
 
             size_freq[:,j+1] = [np.random.binomial(n=n_j[k], p=self.pmort) for k in range(self.nsize)]
-            removed[:,j+1] = [np.random.binomial(size_freq[k,j+1], harvest_rate[k,j+1]) for k in range(self.nsize)]
-            
+            removed[:,j+1] = [np.random.binomial(size_freq[k,j+1], harvest_rate[j+1,k]) for k in range(self.nsize)]
+
+        # get observations and normalize them
         self.observations = np.array([np.sum(removed[:,j]) for j in range(self.ntime)], dtype = np.float32)
+        self.observations = 2 * self.observations / self.max_obs - 1 
 
 
         #calculate new adult population after overwinter mortality
@@ -78,5 +88,33 @@ class greenCrabNineMonth(greenCrabEnv):
         done = bool(self.years_passed > self.Tmax)
 
         return self.observations, self.reward, done, done, {}
+
+    def reward_func(self,action):
+        def month_trap_cost(self, action_at_month: np.float32):
+            cost_of_1 = 1 - (99/self.max_action)**0.8 - ((self.max_action - 100)/self.max_action) ** 4
+            n_traps =  self.max_action * (1+action_at_month) / 2
+            if n_traps < 1:
+                return 0
+            elif 1 <= n_traps < 100:
+                return cost_of_1 + ((n_traps-1) / self.max_action) ** 0.8
+            elif 100 <= n_traps:
+                return cost_of_1 + (99/self.max_action) ** 0.8 + ((n_traps - 100) / self.max_action) ** 4
+
+        costs = np.float32([month_trap_cost(self, action_at_month) for action_at_month in action])
+        total_cost = np.sum(costs) * self.action_reward_scale
+
+        ecological_damage = (
+            self.loss_a / (
+                1+np.exp(
+                    - self.loss_b * (
+                        np.sum(self.state) / self.area - self.loss_c
+                    )
+                )
+            )
+        )
+        
+        reward = - ecological_damage - total_cost
+        print(f"eco: {ecological_damage:.5f}, cost: {total_cost:.5f}")
+        return reward
         
         
