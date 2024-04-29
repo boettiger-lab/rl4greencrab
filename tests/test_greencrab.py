ef6bfee7656a7dc33deff2ac3e952f236736e9ff
Felipe Montealegre-Mora <felimomo@berkeley.edu>
2024-04-29 18:25:22 +0000

    added env check

--- tests/test_greencrab.py
+++ tests/test_greencrab.py
@@ -1,8 +1,9 @@
 # Confirm environment is correctly defined:
 from stable_baselines3.common.env_checker import check_env
-from rl4greencrab import greenCrabEnv, greenCrabSimplifiedEnv, timeSeriesEnv
+from rl4greencrab import greenCrabEnv, greenCrabSimplifiedEnv, greenCrabNineMonth, timeSeriesEnv
 
 def test_GC():
     check_env(greenCrabEnv(), warn=True)
     check_env(greenCrabSimplifiedEnv(), warn=True)
     check_env(timeSeriesEnv(), warn=True)
+    check_env(greenCrabNineMonth(), warn=True)
