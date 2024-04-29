5c90381e1c5faaf7fb408a352165c9bb2a49b089
Felipe Montealegre-Mora <felimomo@berkeley.edu>
2024-04-29 17:53:06 +0000

    env registration

--- src/rl4greencrab/__init__.py
+++ src/rl4greencrab/__init__.py
@@ -1,4 +1,5 @@
 from rl4greencrab.envs.green_crab_ipm import greenCrabEnv, greenCrabSimplifiedEnv
+from rl4greencrab.envs.green_crab_nine_month import greenCrabNineMonth
 from rl4greencrab.envs.time_series import timeSeriesEnv
 from rl4greencrab.agents.const_action import constAction, constActionNatUnits, multiConstAction
 from rl4greencrab.agents.const_escapement import constEsc
@@ -11,6 +12,10 @@ register(
 )
 register(
     id="gcsenv", 
+    entry_point="rl4greencrab.envs.green_crab_ipm:greenCrabSimplifiedEnv"
+)
+register(
+    id="gcnm", 
     entry_point="rl4greencrab.envs.green_crab_ipm:greenCrabSimplifiedEnv"
 )
 register(
