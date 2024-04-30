9df084083fe999695700085535222ad8aaf5c08e
Felipe Montealegre-Mora <felimomo@berkeley.edu>
2024-04-30 17:13:32 +0000

    New sb3 train fn for more flexibility

--- scripts/train.py
+++ scripts/train.py
@@ -1,10 +1,25 @@
 #!/opt/venv/bin/python
+
+# script args
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file", help="Path config file", type=str)
+parser.add_argument("-pb", "--progress_bar", help="Use  progress bar for training", type=bool)
 args = parser.parse_args()
 
-import rl4greencrab
-from rl4greencrab.utils.sb3 import sb3_train 
+# imports
+import rl4fisheries
+from rl4fisheries.utils import sb3_train
 
-sb3_train(args.file)
+import os
+
+# transform to absolute file path
+abs_filepath = os.path.abspath(args.file)
+
+# change directory to script's directory (since io uses relative paths)
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+# train
+sb3_train(abs_filepath, progress_bar = args.progress_bar)
