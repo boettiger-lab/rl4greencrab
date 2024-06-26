06f46326984c45e299051868fda07cb09e254592
Jim Jiang <jiangjingzhi@berkeley.edu>
2024-06-26 02:48:47 +0000

    separate sample_params

--- notebooks/tunning_BG.py
+++ notebooks/tunning_BG.py
@@ -44,6 +44,18 @@ from optuna.pruners import MedianPruner
 from optuna.samplers import TPESampler
 from optuna.visualization import plot_optimization_history, plot_param_importances
 
+from typing import Any, Dict
+
+import optuna
+from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
+from torch import nn as nn
+import gym
+from rl_zoo3 import linear_schedule
+from collections.abc import Callable
+import logging
+from pathlib import Path
+import os
+from sample_params import sample_ppo_params, sample_td3_params, sample_tqc_params
 
 # ## 1. Other envs
 # ---
@@ -74,11 +86,11 @@ N_TRIALS = 100  # Maximum number of trials
 N_JOBS = 1 # Number of jobs to run in parallel
 N_STARTUP_TRIALS = 5  # Stop random sampling after N_STARTUP_TRIALS
 N_EVALUATIONS = 2  # Number of evaluations during the training
-N_TIMESTEPS = 2500000 # Training budget
+N_TIMESTEPS = 1000000 # Training budget
 EVAL_FREQ = int(N_TIMESTEPS / N_EVALUATIONS)
 N_EVAL_ENVS = 12
 N_EVAL_EPISODES = 10
-TIMEOUT = int(180 * 100)  # 5 hrs
+TIMEOUT = int(360 * 100)  # 10 hrs
 
 DEFAULT_HYPERPARAMS = {
     "policy": "MlpPolicy",
@@ -86,240 +98,10 @@ DEFAULT_HYPERPARAMS = {
     "tensorboard_log": "/home/rstudio/logs"
 }
 
+save_tunning_model_name = "tunning_best_gcse_tqc"
+file_name_to_write = 'study_results_tqc_cartpole.csv'
 
-# ### Define Search Space
-
-# %%
-
-
-from typing import Any, Dict
-
-import optuna
-from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
-from torch import nn as nn
-import gym
-from rl_zoo3 import linear_schedule
-from collections.abc import Callable
-import logging
-from pathlib import Path
-import os
-
-def sample_ppo_params(trial: optuna.Trial, n_actions: int, n_envs: int, additional_args: dict) -> Dict[str, Any]:
-    """
-    Sampler for PPO hyperparams.
-
-    :param trial:
-    :return:
-    """
-    batch_size = trial.suggest_categorical("batch_size", [8, 16, 32, 64, 128, 256, 512])
-    n_steps = trial.suggest_categorical("n_steps", [8, 16, 32, 64, 128, 256, 512, 1024, 2048])
-    gamma = trial.suggest_categorical("gamma", [0.9, 0.95, 0.98, 0.99, 0.995, 0.999, 0.9999])
-    learning_rate = trial.suggest_float("learning_rate", 1e-5, 0.1, log=True)
-    ent_coef = trial.suggest_float("ent_coef", 0.00000001, 0.1, log=True)
-    clip_range = trial.suggest_categorical("clip_range", [0.1, 0.2, 0.3, 0.4])
-    n_epochs = trial.suggest_categorical("n_epochs", [1, 5, 10, 20])
-    gae_lambda = trial.suggest_categorical("gae_lambda", [0.8, 0.9, 0.92, 0.95, 0.98, 0.99, 1.0])
-    max_grad_norm = trial.suggest_categorical("max_grad_norm", [0.3, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 2, 5])
-    vf_coef = trial.suggest_float("vf_coef", 0, 1)
-    net_arch_type = trial.suggest_categorical("net_arch", ["tiny", "small", "medium"])
-    # Uncomment for gSDE (continuous actions)
-    # log_std_init = trial.suggest_float("log_std_init", -4, 1)
-    # Uncomment for gSDE (continuous action)
-    # sde_sample_freq = trial.suggest_categorical("sde_sample_freq", [-1, 8, 16, 32, 64, 128, 256])
-    # Orthogonal initialization
-    ortho_init = False
-    # ortho_init = trial.suggest_categorical('ortho_init', [False, True])
-    # activation_fn = trial.suggest_categorical('activation_fn', ['tanh', 'relu', 'elu', 'leaky_relu'])
-    activation_fn_name = trial.suggest_categorical("activation_fn", ["tanh", "relu"])
-    # lr_schedule = "constant"
-    # Uncomment to enable learning rate schedule
-    # lr_schedule = trial.suggest_categorical('lr_schedule', ['linear', 'constant'])
-    # if lr_schedule == "linear":
-    #     learning_rate = linear_schedule(learning_rate)
-
-    # TODO: account when using multiple envs
-    if batch_size > n_steps:
-        batch_size = n_steps
-
-    # Independent networks usually work best
-    # when not working with images
-    net_arch = {
-        "tiny": dict(pi=[64], vf=[64]),
-        "small": dict(pi=[64, 64], vf=[64, 64]),
-        "medium": dict(pi=[256, 256], vf=[256, 256]),
-    }[net_arch_type]
-
-    activation_fn = {"tanh": nn.Tanh, "relu": nn.ReLU, "elu": nn.ELU, "leaky_relu": nn.LeakyReLU}[activation_fn_name]
-    return {
-        "n_steps": n_steps,
-        "batch_size": batch_size,
-        "gamma": gamma,
-        "learning_rate": learning_rate,
-        "ent_coef": ent_coef,
-        "clip_range": clip_range,
-        "n_epochs": n_epochs,
-        "gae_lambda": gae_lambda,
-        "max_grad_norm": max_grad_norm,
-        "vf_coef": vf_coef,
-        # "sde_sample_freq": sde_sample_freq,
-        "policy_kwargs": dict(
-            # log_std_init=log_std_init,
-            net_arch=net_arch,
-            activation_fn=activation_fn,
-            ortho_init=ortho_init,
-        ),
-    }
-
-
-# ### TD3 parameter
-
-# %%
-
-
-def sample_td3_params(trial: optuna.Trial, n_actions: int, n_envs: int, additional_args: dict) -> Dict[str, Any]:
-    """
-    Sampler for TD3 hyperparams.
-
-    :param trial:
-    :return:
-    """
-    gamma = trial.suggest_categorical("gamma", [0.9, 0.95, 0.98, 0.99, 0.995, 0.999, 0.9999])
-    #learning_rate = trial.suggest_float("learning_rate", 1e-5, 1, log=True)
-    learning_rate = trial.suggest_float("learning_rate", 1e-5, 0.1, log=True)
-    batch_size = trial.suggest_categorical("batch_size", [16, 32, 64, 100, 128, 256, 512, 1024, 2048])
-    buffer_size = trial.suggest_categorical("buffer_size", [int(1e4), int(1e5), int(1e6)])
-    # Polyak coeff
-    tau = trial.suggest_categorical("tau", [0.001, 0.005, 0.01, 0.02, 0.05, 0.08])
-
-    train_freq = trial.suggest_categorical("train_freq", [1, 4, 8, 16, 32, 64, 128, 256, 512])
-    gradient_steps = train_freq
-
-    noise_type = trial.suggest_categorical("noise_type", ["ornstein-uhlenbeck", "normal", None])
-    noise_std = trial.suggest_float("noise_std", 0, 1)
-
-    # NOTE: Add "verybig" to net_arch when tuning HER
-    net_arch_type = trial.suggest_categorical("net_arch", ["small", "medium", "big"])
-    # activation_fn = trial.suggest_categorical('activation_fn', [nn.Tanh, nn.ReLU, nn.ELU, nn.LeakyReLU])
-
-    net_arch = {
-        "small": [64, 64],
-        "medium": [256, 256],
-        "big": [400, 300],
-        # Uncomment for tuning HER
-        # "verybig": [256, 256, 256],
-    }[net_arch_type]
-
-    hyperparams = {
-        "gamma": gamma,
-        "learning_rate": learning_rate,
-        "batch_size": batch_size,
-        "buffer_size": buffer_size,
-        "train_freq": train_freq,
-        "gradient_steps": gradient_steps,
-        "policy_kwargs": dict(net_arch=net_arch),
-        "tau": tau,
-    }
-
-    """if noise_type == "normal":
-        hyperparams["action_noise"] = NormalActionNoise(mean=np.zeros(n_actions), sigma=noise_std * np.ones(n_actions))
-    elif noise_type == "ornstein-uhlenbeck":
-        hyperparams["action_noise"] = OrnsteinUhlenbeckActionNoise(
-            mean=np.zeros(n_actions), sigma=noise_std * np.ones(n_actions)
-        )"""
-
-    """if additional_args["using_her_replay_buffer"]:
-        hyperparams = sample_her_params(trial, hyperparams, additional_args["her_kwargs"])"""
-
-    return hyperparams
-
-
-# ### TQC parameter
-
-# %%
-
-
-def sample_sac_params(trial: optuna.Trial, n_actions: int, n_envs: int, additional_args: dict) -> Dict[str, Any]:
-    """
-    Sampler for SAC hyperparams.
-
-    :param trial:
-    :return:
-    """
-    gamma = trial.suggest_categorical("gamma", [0.9, 0.95, 0.98, 0.99, 0.995, 0.999, 0.9999])
-    #learning_rate = trial.suggest_float("learning_rate", 1e-5, 1, log=True)
-    learning_rate = trial.suggest_float("learning_rate", 1e-5, 0.1, log=True)
-    batch_size = trial.suggest_categorical("batch_size", [16, 32, 64, 128, 256, 512, 1024, 2048])
-    buffer_size = trial.suggest_categorical("buffer_size", [int(1e4), int(1e5), int(1e6)])
-    learning_starts = trial.suggest_categorical("learning_starts", [0, 1000, 10000, 20000])
-    # train_freq = trial.suggest_categorical('train_freq', [1, 10, 100, 300])
-    train_freq = trial.suggest_categorical("train_freq", [1, 4, 8, 16, 32, 64, 128, 256, 512])
-    # Polyak coeff
-    tau = trial.suggest_categorical("tau", [0.001, 0.005, 0.01, 0.02, 0.05, 0.08])
-    # gradient_steps takes too much time
-    # gradient_steps = trial.suggest_categorical('gradient_steps', [1, 100, 300])
-    gradient_steps = train_freq
-    # ent_coef = trial.suggest_categorical('ent_coef', ['auto', 0.5, 0.1, 0.05, 0.01, 0.0001])
-    ent_coef = "auto"
-    # You can comment that out when not using gSDE
-    log_std_init = trial.suggest_float("log_std_init", -4, 1)
-    # NOTE: Add "verybig" to net_arch when tuning HER
-    net_arch_type = trial.suggest_categorical("net_arch", ["small", "medium", "big"])
-    # activation_fn = trial.suggest_categorical('activation_fn', [nn.Tanh, nn.ReLU, nn.ELU, nn.LeakyReLU])
-
-    net_arch = {
-        "small": [64, 64],
-        "medium": [256, 256],
-        "big": [400, 300],
-        # Uncomment for tuning HER
-        # "large": [256, 256, 256],
-        # "verybig": [512, 512, 512],
-    }[net_arch_type]
-
-    target_entropy = "auto"
-    # if ent_coef == 'auto':
-    #     # target_entropy = trial.suggest_categorical('target_entropy', ['auto', 5, 1, 0, -1, -5, -10, -20, -50])
-    #     target_entropy = trial.suggest_float('target_entropy', -10, 10)
-
-    hyperparams = {
-        "gamma": gamma,
-        "learning_rate": learning_rate,
-        "batch_size": batch_size,
-        "buffer_size": buffer_size,
-        "learning_starts": learning_starts,
-        "train_freq": train_freq,
-        "gradient_steps": gradient_steps,
-        "ent_coef": ent_coef,
-        "tau": tau,
-        "target_entropy": target_entropy,
-        "policy_kwargs": dict(log_std_init=log_std_init, net_arch=net_arch),
-    }
-
-    """if additional_args["using_her_replay_buffer"]:
-        hyperparams = sample_her_params(trial, hyperparams, additional_args["her_kwargs"])"""
-
-    return hyperparams
-
-
-# %%
-
-
-def sample_tqc_params(trial: optuna.Trial, n_actions: int, n_envs: int, additional_args: dict) -> Dict[str, Any]:
-    """
-    Sampler for TQC hyperparams.
-
-    :param trial:
-    :return:
-    """
-    # TQC is SAC + Distributional RL
-    hyperparams = sample_sac_params(trial, n_actions, n_envs, additional_args)
-
-    n_quantiles = trial.suggest_int("n_quantiles", 5, 50)
-    top_quantiles_to_drop_per_net = trial.suggest_int("top_quantiles_to_drop_per_net", 0, n_quantiles - 1)
-
-    hyperparams["policy_kwargs"].update({"n_quantiles": n_quantiles})
-    hyperparams["top_quantiles_to_drop_per_net"] = top_quantiles_to_drop_per_net
-
-    return hyperparams
+# ### Define Search Space in another sample_params.py
 
 
 # ### define objective function
@@ -380,68 +162,6 @@ class TrialEvalCallback(EvalCallback):
 # %%
 
 
-# objective function for PPO only
-def objective(trial: optuna.Trial) -> float:
-    """
-    Objective function using by Optuna to evaluate
-    one configuration (i.e., one set of hyperparameters).
-
-    Given a trial object, it will sample hyperparameters,
-    evaluate it and report the result (mean episodic reward after training)
-
-    :param trial: Optuna trial object
-    :return: Mean episodic reward after training
-    """
-
-    kwargs = DEFAULT_HYPERPARAMS.copy() 
-    ### YOUR CODE HERE
-    # TODO: 
-    # 1. Sample hyperparameters and update the default keyword arguments: `kwargs.update(other_params)`
-    # 2. Create the evaluation envs
-    # 3. Create the `TrialEvalCallback`
-
-    # 1. Sample hyperparameters and update the keyword arguments
-    kwargs.update(sample_ppo_params(trial,N_EVAL_ENVS))
-
-    # Create the RL model
-    model = PPO(**kwargs)
-
-    # 2. Create envs used for evaluation using `make_vec_env`, `ENV_ID` and `N_EVAL_ENVS`
-    eval_env = greenCrabSimplifiedEnv()
-    # 3. Create the `TrialEvalCallback` callback defined above that will periodically evaluate
-    # and report the performance using `N_EVAL_EPISODES` every `EVAL_FREQ`
-    # TrialEvalCallback signature:
-    # TrialEvalCallback(eval_env, trial, n_eval_episodes, eval_freq, deterministic, verbose)
-    eval_callback = TrialEvalCallback(eval_env, trial, N_EVAL_EPISODES, EVAL_FREQ, deterministic=True)
-
-    ### END OF YOUR CODE
-
-    nan_encountered = False
-    try:
-        # Train the model
-        model.learn(N_TIMESTEPS, callback=eval_callback)
-    except AssertionError as e:
-        # Sometimes, random hyperparams can generate NaN
-        print(e)
-        nan_encountered = True
-    finally:
-        # Free memory
-        model.env.close()
-        vec_env.close()
-
-    # Tell the optimizer that the trial failed
-    if nan_encountered:
-        return float("nan")
-
-    if eval_callback.is_pruned:
-        raise optuna.exceptions.TrialPruned()
-
-    return eval_callback.last_mean_reward
-
-
-# %%
-
-
 # objective all models
 def objective_flex(model_parameter, model_train):
     """
@@ -475,8 +195,6 @@ def objective_flex(model_parameter, model_train):
         # TrialEvalCallback(eval_env, trial, n_eval_episodes, eval_freq, deterministic, verbose)
         eval_callback = TrialEvalCallback(eval_env, trial, N_EVAL_EPISODES, EVAL_FREQ, deterministic=True)
     
-        ### END OF YOUR CODE
-    
         nan_encountered = False
         try:
             # Train the model
@@ -487,10 +205,10 @@ def objective_flex(model_parameter, model_train):
             nan_encountered = True
         finally:
             # Check if the current trial has the best accuracy
-            if trial.number >= 2:
-                if eval_callback.last_mean_reward > study.best_trial:
+            if trial.number >= 1:
+                if eval_callback.last_mean_reward > study.best_trial.value:
                     print("save the current best model")
-                    model.save("tunning_best_gcse") # save the trained best model
+                    model.save(save_tunning_model_name) # save the trained best model
             # Free memory
             model.env.close()
             vec_env.close()
@@ -540,7 +258,7 @@ study = optuna.create_study(sampler=sampler, pruner=pruner, direction="maximize"
 
 try:
     print("start the training")
-    study.optimize(objective_PPO, n_trials=N_TRIALS, n_jobs=N_JOBS, timeout=TIMEOUT)
+    study.optimize(objective_TQC, n_trials=N_TRIALS, n_jobs=N_JOBS, timeout=TIMEOUT)
 except KeyboardInterrupt:
     pass
 
@@ -559,7 +277,6 @@ print("  User attrs:")
 for key, value in trial.user_attrs.items():
     print(f"    {key}: {value}")
 
-file_name_to_write = 'study_results_ppo_cartpole.csv'
 DF_file = Path(f" ./{file_name_to_write}")
 
 if not os.path.isfile(file_name_to_write):
