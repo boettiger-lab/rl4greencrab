e46a1eb988f2733ea8e1c48d0acfa02c19aff7a9
Jim Jiang <jiangjingzhi@berkeley.edu>
2024-12-02 17:30:21 +0000

    month env train

---
+++ notebooks/simple_train.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+from stable_baselines3 import PPO, TD3
+from sb3_contrib import TQC, RecurrentPPO
+from stable_baselines3.common.env_util import make_vec_env
+from rl4greencrab.envs.green_crab_monthly_env import greenCrabMonthEnv
+import gymnasium as gym
+import logging
+
+print('start training')
+
+config = {}
+
+gcme = greenCrabMonthEnv()
+vec_env = make_vec_env(greenCrabMonthEnv, n_envs=12)
+
+def model_train(model_name):
+    model = PPO("MlpPolicy", gcme, verbose=0, tensorboard_log="/home/rstudio/logs") #defualt PPO
+    if model_name == 'PPO':
+        model = PPO("MlpPolicy", gcme, verbose=0, tensorboard_log="/home/rstudio/logs")
+    elif model_name == 'TQC':
+        model = TQC("MlpPolicy", gcme, verbose=0, tensorboard_log="/home/rstudio/logs")
+    elif model_name == 'TD3':
+        model = TD3("MlpPolicy", gcme, verbose=0, tensorboard_log="/home/rstudio/logs")
+    elif model_name == 'RecurrentPPO':
+        model = RecurrentPPO("MlpLstmPolicy", gcme, verbose=0, tensorboard_log="/home/rstudio/logs")
+
+    print(f'start train {model_name}')
+    model.learn(
+            total_timesteps= 1000000, 
+            progress_bar=False,
+        )
+    model_path = model_name + '_gcme'
+    model.save(model_path)
+    
+#model_train('PPO')
+#model_train('TQC')
+# model_train('TD3')
+model_train('RecurrentPPO')
+
+print("finish training")
