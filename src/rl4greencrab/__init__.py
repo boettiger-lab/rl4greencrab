7bd1485364630cb1363ddcac4b1ab3bee490f442
Jim Jiang <jiangjingzhi@berkeley.edu>
2024-12-30 19:10:17 +0000

    add monthly normalized env

--- src/rl4greencrab/__init__.py
+++ src/rl4greencrab/__init__.py
@@ -1,6 +1,7 @@
 from rl4greencrab.envs.green_crab_ipm import greenCrabEnv, greenCrabSimplifiedEnv
 from rl4greencrab.envs.time_series import timeSeriesEnv
 from rl4greencrab.envs.green_crab_monthly_env import greenCrabMonthEnv
+from rl4greencrab.envs.green_crab_monthly_env_norm import greenCrabMonthEnvNormalized
 from rl4greencrab.agents.const_action import constAction, constActionNatUnits, multiConstAction
 from rl4greencrab.agents.const_escapement import constEsc
 from rl4greencrab.utils.simulate import simulator, get_simulator, evaluate_agent
@@ -21,4 +22,8 @@ register(
 register(
     id="monthenv", 
     entry_point="rl4greencrab.envs.green_crab_monthly_env:greenCrabMonthEnv",
+)
+register(
+    id="monthenvnorm", 
+    entry_point="rl4greencrab.envs.green_crab_monthly_env_norm:greenCrabMonthEnvNormalized",
 )
