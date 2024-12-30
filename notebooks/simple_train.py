698843c4e26a644bddbc7aff5eb4cea03075033d
Jim Jiang <jiangjingzhi@berkeley.edu>
2024-12-30 18:57:40 +0000

    save recent change in model

--- notebooks/simple_train.py
+++ notebooks/simple_train.py
@@ -4,6 +4,7 @@ from stable_baselines3 import PPO, TD3
 from sb3_contrib import TQC, RecurrentPPO
 from stable_baselines3.common.env_util import make_vec_env
 from rl4greencrab.envs.green_crab_monthly_env import greenCrabMonthEnv
+from rl4greencrab.envs.green_crab_monthly_env_norm import greenCrabMonthEnvNormalized
 import gymnasium as gym
 import logging
 
@@ -12,30 +13,31 @@ print('start training')
 config = {}
 
 gcme = greenCrabMonthEnv()
-vec_env = make_vec_env(greenCrabMonthEnv, n_envs=12)
+gmonthNorm = greenCrabMonthEnvNormalized()
+vec_env = make_vec_env(greenCrabMonthEnvNormalized, n_envs=12)
 
 def model_train(model_name):
-    model = PPO("MlpPolicy", gcme, verbose=0, tensorboard_log="/home/rstudio/logs") #defualt PPO
+    model = PPO("MlpPolicy", vec_env, verbose=0, tensorboard_log="/home/rstudio/logs") #defualt PPO
     if model_name == 'PPO':
-        model = PPO("MlpPolicy", gcme, verbose=0, tensorboard_log="/home/rstudio/logs")
+        model = PPO("MlpPolicy", vec_env, verbose=0, tensorboard_log="/home/rstudio/logs")
     elif model_name == 'TQC':
-        model = TQC("MlpPolicy", gcme, verbose=0, tensorboard_log="/home/rstudio/logs")
+        model = TQC("MlpPolicy", vec_env, verbose=0, tensorboard_log="/home/rstudio/logs")
     elif model_name == 'TD3':
-        model = TD3("MlpPolicy", gcme, verbose=0, tensorboard_log="/home/rstudio/logs")
+        model = TD3("MlpPolicy", vec_env, verbose=0, tensorboard_log="/home/rstudio/logs")
     elif model_name == 'RecurrentPPO':
-        model = RecurrentPPO("MlpLstmPolicy", gcme, verbose=0, tensorboard_log="/home/rstudio/logs")
+        model = RecurrentPPO("MlpLstmPolicy", vec_env, verbose=0, tensorboard_log="/home/rstudio/logs")
 
     print(f'start train {model_name}')
     model.learn(
-            total_timesteps= 1000000, 
+            total_timesteps= 3000000, 
             progress_bar=False,
         )
-    model_path = model_name + '_gcme'
+    model_path = model_name + '_gcmenorm'
     model.save(model_path)
     
-#model_train('PPO')
-#model_train('TQC')
-# model_train('TD3')
-model_train('RecurrentPPO')
+# model_train('PPO')
+# model_train('TQC')
+model_train('TD3')
+#model_train('RecurrentPPO')
 
 print("finish training")
