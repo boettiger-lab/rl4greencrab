40c771ab696d8e2ad91de8a9f6d2a8cde83ccd4c
Jim Jiang <jiangjingzhi@berkeley.edu>
2025-03-31 01:13:49 +0000

    update env

--- src/rl4greencrab/envs/green_crab_monthly_env_norm.py
+++ src/rl4greencrab/envs/green_crab_monthly_env_norm.py
@@ -11,11 +11,20 @@ logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 class greenCrabMonthEnvNormalized(greenCrabMonthEnv):
     def __init__(self, config={}):
         super().__init__(config=config)
-        self.observation_space = spaces.Box(
-            np.array([-1], dtype=np.float32),
-            np.array([1], dtype=np.float32),
-            dtype=np.float32,
-        )
+        # self.observation_space = spaces.Box(
+        #     np.array([-1], dtype=np.float32),
+        #     np.array([1], dtype=np.float32),
+        #     dtype=np.float32,
+        # )
+        self.observation_space = spaces.Dict({
+            "crabs": spaces.Box(
+                low=np.array([-1, -1]),  # Lower bounds: original obs (0), month (1)
+                high=np.array([1, 1]),  # Upper bounds: obs max, month max (12)
+                dtype=np.float32
+            ), 
+            "months": spaces.Discrete(12, start=1)
+        })
+        
         self.action_space = spaces.Box(
             np.float32([-1, -1, -1]),
             np.float32([1, 1, 1]),
@@ -29,18 +38,27 @@ class greenCrabMonthEnvNormalized(greenCrabMonthEnv):
         obs, rew, term, trunc, info = super().step(
             np.float32(action_natural_units)
         )
-        normalized_cpue = 2 * self.cpue_2(obs, action_natural_units) - 1
-        # observation = np.float32(np.append(normalized_cpue, action))
-        observation = normalized_cpue
+        normalized_cpue = 2 * self.cpue_2(obs['crabs'], action_natural_units) - 1
+
+        mean_biomass = obs["crabs"][1]
+        normal_biomass = self.normalize_biomass(mean_biomass)
+        # TODO: normalize biomass
+        self.observation = {"crabs": np.array([normalized_cpue[0], normal_biomass]), "months": obs['months']}
         # rew = 10 * rew # use larger rewards, possibly makes trainer easier?
-        return observation, rew, term, trunc, info
+        return self.observation, rew, term, trunc, info
 
     def reset(self, *, seed=42, options=None):
         _, info = super().reset(seed=seed, options=options)
 
         # completely new  obs
-        return - np.ones(shape=self.observation_space.shape, dtype=np.float32), info
-        
+        return {"crabs":np.array([-1, -1]), "months":1}, info
+
+    def normalize_biomass(self, mean_biomass):
+        biomass_sizes = self.get_biomass_size()
+        b0 =  biomass_sizes[0]
+        b20 = biomass_sizes[-1]
+        return -1 + 2 * (mean_biomass - b0)/(b20 - b0)
+    
     def cpue_2(self, obs, action_natural_units):
         # If you don't set traps, the catch-per-effort is 0/0.  Should be NaN, but we call it 0
         if np.sum(action_natural_units) <= 0:
