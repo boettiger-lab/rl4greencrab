da0e5d08a9cb801e9b4fb4f522801ba555523a9b
Jim Jiang <jiangjingzhi@berkeley.edu>
2025-06-05 04:46:12 +0000

    recurrentPPO training

--- notebooks/simple_train.py
+++ notebooks/simple_train.py
@@ -13,12 +13,38 @@ print('start training',flush=True)
 config = {
     "w_mort_scale" : 600,
     "growth_k": 0.70,
-    "random_start": True
+    "random_start": True,
+    "curriculum": False
 }
 
 gcme = greenCrabMonthEnv(config)
 gmonthNorm = greenCrabMonthEnvNormalized(config)
 vec_env = make_vec_env(greenCrabMonthEnvNormalized, n_envs=12, env_kwargs={'config':config})
+
+model_config = {
+    'policy':"MultiInputLstmPolicy",
+    'env':vec_env,
+    'verbose':0,
+    'normalize_advantage': True,
+    'batch_size': 256,
+    'n_steps': 1024,
+    'gamma': 0.9999,
+    'learning_rate': 0.0003,
+    'ent_coef': 0.00429,
+    'clip_range': 0.1,
+    'n_epochs': 10,
+    'gae_lambda': 0.9,
+    'max_grad_norm': 0.5,
+    'vf_coef': 0.19,
+    'use_sde': False,
+    'sde_sample_freq': 8,
+    'tensorboard_log':"/home/rstudio/logs",
+    'policy_kwargs': dict(log_std_init=0.0, ortho_init=False,
+                       lstm_hidden_size=128,
+                       n_lstm_layers = 2,
+                       enable_critic_lstm=True,
+                       net_arch=[64, 32, 16])
+}
 
 def model_train(model_name):
     model = PPO('MultiInputPolicy', vec_env, verbose=0, tensorboard_log="/home/rstudio/logs") #defualt PPO
@@ -29,14 +55,21 @@ def model_train(model_name):
     elif model_name == 'TD3':
         model = TD3('MultiInputPolicy', vec_env, verbose=0, tensorboard_log="/home/rstudio/logs")
     elif model_name == 'RecurrentPPO':
-        model = RecurrentPPO("MultiInputLstmPolicy", vec_env, verbose=0, tensorboard_log="/home/rstudio/logs")
+        model = RecurrentPPO(**model_config)
 
     print(f'start train {model_name}', flush=True)
+    
+    lstm_hidden_size = model_config['policy_kwargs']['lstm_hidden_size']
+    n_lstm_layers = model_config['policy_kwargs']['n_lstm_layers']
+    net_arch = model_config['policy_kwargs']['net_arch']
+    use_sde = model_config['use_sde']
+    model_path = f'{model_name}_gcmenorm_{lstm_hidden_size}_{n_lstm_layers}_{net_arch}_{use_sde}'
+    print(model_path, flush=True)    
+    
     model.learn(
-            total_timesteps= 3000000, 
+            total_timesteps= 10000000,
             progress_bar=False,
         )
-    model_path = model_name + '_gcmenorm'
     model.save(model_path)
     
 # model_train('PPO')
