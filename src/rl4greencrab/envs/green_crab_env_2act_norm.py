fix mean_biomass calculation bug in greencrab monthly env

src/rl4greencrab/envs/green_crab_env_2act_norm.py
@@ -38,7 +38,7 @@ class greenCrabMonthEnvTwoActNormalized(greenCrabMonthEnvTwoAct):
         normalized_cpue = 2 * self.cpue_2(obs['crabs'], action_natural_units) - 1
         mean_biomass = obs["crabs"][1]
         normal_biomass = self.normalize_biomass(mean_biomass)
-        # TODO: normalize biomass
+        
         self.observation = {"crabs": np.array([normalized_cpue[0], normal_biomass], dtype=np.float32), 
                             "months": obs['months']}
         # rew = 10 * rew # use larger rewards, possibly makes trainer easier?

src/rl4greencrab/envs/green_crab_monthly_env.py
@@ -172,7 +172,7 @@ class greenCrabMonthEnv(gym.Env):
         self.action_stacks.append(normalized_action)
         
         #update observation space
-        biomass = np.sum(self.get_biomass_size() * self.state) # get biomass
+        biomass = np.sum(self.get_biomass_size() * removed[:,0]) # get biomass
         crab_counts = np.sum(removed[:,0])
         mean_biomass = biomass/crab_counts if crab_counts != 0 else 0
         self.observations = {"crabs": np.array([crab_counts, mean_biomass], dtype=np.float32), 